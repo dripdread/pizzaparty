35845b7daf7d9b91e98001cdecc6904b63151fe3
Julian M. Kunkel <juliankunkel@googlemail.com>
2020-10-05 16:55:46 +0100

    Update

--- lecture-examples/autumn/6/compute-squares-of-even.c
+++ lecture-examples/autumn/6/compute-squares-of-even.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
-// This program should sum up the square of even numbers of i=1 to i=10
-// The result should be sum = 2^2 + 4^2 +...+ 10^2
+// This program should sum up the square of all even numbers between i=1 to i=10
+// The result should be sum = 2^2 + 4^2 + ...+ 10^2
 int main(){
   int sum = 0;
+
   for(int i=1; i <= 10; i++){
-    if(i % 2 == 0){
+    if(i % 2 == 0){ // check for an even number
       sum += i*i;
     }
   }
-  printf("sum of even numbers = %d\n", sum);
+  printf("sum = %d\n", sum);
 
-  // alternative solution
+  // Alternative approach
   sum = 0;
   for(int i=2; i <= 10; i += 2){
     sum += i*i;
   }
-  printf("sum of even numbers = %d\n", sum);
+  printf("sum = %d\n", sum);
   return 0;
 }
