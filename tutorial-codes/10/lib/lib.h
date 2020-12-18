20e16101ac2bf908f61b527d34614b5b22050e1c
Julian M. Kunkel <juliankunkel@googlemail.com>
2020-12-18 14:33:37 +0000

    Code mvoed

--- tutorial-codes/10/lib/lib.h
+++ tutorial-codes/10/lib/lib.h
@@ -8,5 +8,4 @@ typedef struct my_struct_t my_struct_t; // forward declaration, allowed for poin
 my_struct_t * lib_test_init(char const * str);
 void lib_do_something(bool t, my_struct_t * val);
 char const * lib_test_get_str(my_struct_t * val);
-
 #endif
