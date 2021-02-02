fea6d236f1268e00babb3f73f64c139df60348f1
Julian M. Kunkel <juliankunkel@googlemail.com>
2021-02-02 15:10:48 +0000

    Other codes

---
+++ tutorial-codes/spring/4/w4ex1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+class UoR
+{
+public:
+
+	void func(int x)
+	{
+		cout << "value of x is " << x << endl;
+	}
+
+	void func(double x)
+	{
+		cout << "value of x is " << x << endl;
+	}
+
+	void func(int x, int y)
+	{
+		cout << "value of x and y is " << x << ", " << y << endl;
+	}
+};
+
+int main() {
+
+	UoR obj1;
+	obj1.func(7);
+	obj1.func(9.132);
+	obj1.func(85, 64);
+	return 0;
+};
---
+++ tutorial-codes/spring/4/w4ex5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>  
+using namespace std;
+template<class T> T add(T& a, T& b)
+{
+    T result = a + b;
+    return result;
+}
+
+int main()
+{
+    int i = 2, j = 3;
+    float m = 2.3, n = 1.2;
+    cout << "Addition of i and j is :" << add(i, j);
+    cout << '\n';
+    cout << "Addition of m and n is :" << add(m, n);
+    return 0;
+}
