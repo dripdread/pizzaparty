7a1e835521126656e390d73743af80ea72dc04c0
Julian M. Kunkel <juliankunkel@googlemail.com>
2021-03-02 14:34:24 +0000

    Project report code

---
+++ project/latex/code/example.cpp
@@ -0,0 +1,22 @@
+static void capFrameRate(long *then, float *remainder){
+	long wait, frameTime;
+  // This is a comment
+	wait = 16 + *remainder;
+
+	*remainder -= (int)*remainder;
+
+	frameTime = SDL_GetTicks() - *then;
+
+	wait -= frameTime;
+
+	if (wait < 1)
+	{
+		wait = 1;
+	}
+
+	SDL_Delay(wait);
+
+	*remainder += 0.667;
+
+	*then = SDL_GetTicks();
+}
